f4e4f286d4430387dcde271bfa5732cf93149ba6
elproff <62444739+elproff@users.noreply.github.com>
2023-10-06 17:09:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 const pdfList = document.getElementById("pdf-list");
 
-fetch("https://api.github.com/repos/elproff/agri/contents/book.pdf")
+// Replace 'USERNAME' with your GitHub username and 'REPOSITORY' with your repository name
+fetch("https://api.github.com/repos/elproff/agri/contents/")
     .then(response => response.json())
     .then(data => {
-        const listItem = document.createElement("li");
-        const pdfLink = document.createElement("a");
-        pdfLink.href = data.download_url;
-        pdfLink.textContent = "book.pdf";
-        pdfLink.setAttribute("target", "_blank");
-        listItem.appendChild(pdfLink);
-        pdfList.appendChild(listItem);
+        data.forEach(item => {
+            if (item.name.endsWith(".pdf")) {
+                const listItem = document.createElement("li");
+                const pdfLink = document.createElement("a");
+                pdfLink.href = item.download_url;
+                pdfLink.textContent = item.name;
+                pdfLink.setAttribute("target", "_blank");
+                listItem.appendChild(pdfLink);
+                pdfList.appendChild(listItem);
+            }
+        });
     })
     .catch(error => {
         console.error("Error:", error);
