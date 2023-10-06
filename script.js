033cca7738f00c1d90e84d2b2b8cfdd6da79749e
elproff <62444739+elproff@users.noreply.github.com>
2023-10-06 17:00:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const pdfList = document.getElementById("pdf-list");
 
-// Replace 'USERNAME' with your GitHub username and 'REPOSITORY' with your repository name
-fetch("https://api.github.com/repos/elproff/agri/contents/")
+// Replace 'GOOGLE_DRIVE_FOLDER_LINK' with the shareable link of your specific folder on Google Drive
+const googleDriveFolderLink = "https://drive.google.com/drive/folders/1gFH-9IKMm20MuIKv_1NZwh9hU2CSJ-DH?usp=sharing";
+
+// Use the Google Drive API to list files in the folder
+fetch(`https://www.googleapis.com/drive/v3/files?q='${googleDriveFolderLink}' in parents&key=YOUR_GOOGLE_API_KEY`)
     .then(response => response.json())
     .then(data => {
-        data.forEach(item => {
-            if (item.name.endsWith(".pdf")) {
+        data.files.forEach(file => {
+            if (file.mimeType === "application/pdf") {
                 const listItem = document.createElement("li");
                 const pdfLink = document.createElement("a");
-                pdfLink.href = item.download_url;
-                pdfLink.textContent = item.name;
+                pdfLink.href = `https://drive.google.com/uc?export=download&id=${file.id}`;
+                pdfLink.textContent = file.name;
                 pdfLink.setAttribute("target", "_blank");
                 listItem.appendChild(pdfLink);
                 pdfList.appendChild(listItem);
